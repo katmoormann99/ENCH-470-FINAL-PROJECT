ddc2144826ead9a832afe0fb94cf1ee00f816037
katmoormann99 <91487157+katmoormann99@users.noreply.github.com>
2021-11-29 16:05:56 -0500

    Add files via upload

---
+++ ENCH470FinalProject.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+daily_aqi_by_county_ = pd.read_csv('daily_aqi_by_county_2020.csv')
+
+
+# In[3]:
+
+
+display(daily_aqi_by_county_)
+
+
+# In[4]:
+
+
+daily_aqi_by_county_.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
